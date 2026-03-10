054ff817fa5081872ac0b5b46f0ca26a560564f8
Jeff Blankenburg <github@jeffblankenburg.com>
2026-03-10 17:26:59 -0400

    Update service-worker-register.tsx

--- src/components/service-worker-register.tsx
+++ src/components/service-worker-register.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 export function ServiceWorkerRegister() {
   useEffect(() => {
-    if ("serviceWorker" in navigator) {
+    if ("serviceWorker" in navigator && process.env.NODE_ENV === "production") {
       navigator.serviceWorker.register("/sw.js").catch((err) => {
         console.error("Service worker registration failed:", err);
       });
